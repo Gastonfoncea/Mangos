00f00eca49e460b7fb2c12cf990a82d02e2dbac4
Gaston Foncea <gastonfoncea.09@gmail.com>
2024-04-08 21:03:02 -0300

    Feat: Se actualizaron la tabla de iconos (no se itero todavia)

--- Mangos/Enums/EnumIconos.swift
+++ Mangos/Enums/EnumIconos.swift
@@ -10,4 +10,22 @@ import Foundation
 enum Iconos: String {
     case dolar = "dollarsign"
     case profile = "person.fill"
+    case ingresosSueldo = "dock.arrow.down.rectangle"
+    case gastosComida = "takeoutbag.and.cup.and.straw.fill"
+    case ahorros = "handbag.fill"
+    case tarjetas = "creditcard.fill"
+    case gimnasio = "dumbbell.fill"
+    case cuidadoP = "hand.thumbsup.fill"
+    case estudios = "text.book.closed.fill"
+    case ropa = "hanger"
+    case salidas = "popcorn.fill"
+    case restaurants = "fork.knife"
 }
+
+/*
+ Equivalencias :
+ comida - Gastos - fastFood
+ ingresos - sueldo
+ */
+
+
